755a272e84d1f0d1f60cde3f388813f37f74cc5e
sebbyapudo <sebbyapudo@gmail.com>
2023-06-05 23:47:17 +0300

    Deleted the project | I'm set to begin again

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// Global variable declaration
-const navBtn = document.querySelector('.nav-btn');
-const navLinks = document.querySelector('.nav-links');
-
-navBtn.addEventListener('click', ()=>{
-  navLinks.classList.toggle('show-links');
-});
