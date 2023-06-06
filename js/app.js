bf5368ec27511f343606a42928ac71cd81afede0
sebbyapudo <sebbyapudo@gmail.com>
2023-06-06 21:53:07 +0300

    Implemented the toggle functionality in the hamburger menu

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Declaring global variables
+
+const navBtn = document.querySelector('.nav-btn');
+const navLinks = document.querySelector('.nav-links');
+
+navBtn.addEventListener('click', ()=>{
+  navLinks.classList.toggle('show-links')
+});
