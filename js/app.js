a91b93e6644ba035068c0dba8e41cc0f1b0bfa3a
sebbyapudo <sebbyapudo@gmail.com>
2023-06-25 15:29:07 +0300

    Removed unused code

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// Declaring global variables
-
-const navBtn = document.querySelector('.nav-btn');
-const navLinks = document.querySelector('.nav-links');
-
-navBtn.addEventListener('click', ()=>{
-  navLinks.classList.toggle('show-links')
-});
