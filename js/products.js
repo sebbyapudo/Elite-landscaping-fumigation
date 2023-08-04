ea9ba703232f2dc305875784950e6d1f3828affe
sebbyapudo <sebbyapudo@gmail.com>
2023-08-05 02:55:30 +0300

    Added and updated the blueprint of products js

---
+++ js/products.js
@@ -0,0 +1,54 @@
+const products = [{
+  id: '1',
+  img: './assets/lawn-mower.jp',
+  name: 'Green Pemba Grass',
+  price: '1300/= per sac',
+},
+{
+  id: '2',
+  img: './assets/lawn-mower.jp',
+  name: 'Variegated pemba',
+  price: '1300/= per sac',
+},
+{
+  id: '3',
+  img: './assets/lawn-mower.jp',
+  name: 'Bermuda grass seeds',
+  price: '4500/= 0.5kg',
+},
+{
+  id: '4',
+  img: './assets/lawn-mower.jp',
+  name: 'Arabic grass seeds',
+  price: '1500/= per sac',
+},
+{
+  id: '5',
+  img: './assets/lawn-mower.jp',
+  name: 'Zimbabwe grass seeds',
+  price: '1300/= 0.5kg',
+},
+{
+  id: '6',
+  img: './assets/lawn-mower.jp',
+  name: 'Paspalum grass seeds',
+  price: '5000/= 0.5kg',
+},
+{
+  id: '7',
+  img: './assets/lawn-mower.jp',
+  name: 'Kikuyu grass seeds',
+  price: '1300/= 0.5kg',
+},
+{
+  id: '8',
+  img: './assets/lawn-mower.jpg',
+  name: 'Bent grass seeds',
+  price: '7000/=  0.5kg'
+}, 
+{
+  id: '9',
+  img: './assets/lawn-mower.jpg',
+  name: 'Evergreen grass seeds',
+  price: '5000/=  0.5kg'
+}]
