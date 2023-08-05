7383218a020193067d8c9a110366fc00d6b559a5
sebbyapudo <sebbyapudo@gmail.com>
2023-08-05 04:39:48 +0300

    added and updated the services array object

---
+++ js/services.js
@@ -0,0 +1,48 @@
+const services = [{
+  id: '1',
+  name: 'Landscaping',
+  img: './assets/gardening.jpg',
+  description: 'Elevate your surroundings with our exquisite landscaping services. 🌿🏞️'
+},
+{
+  id: '2',
+  name: 'Fumigation',
+  img: './assets/gardening.jpg',
+  description: 'Effective pest control solutions for a safe and pest-free environment. 🐜🚫'
+},
+{
+  id: '3',
+  name: 'General cleaning',
+  img: './assets/gardening.jpg',
+  description: 'Revitalize your space with our comprehensive cleaning services. 🧹✨'
+},
+{
+  id: '4',
+  name: 'Sofa set cleaning',
+  img: './assets/gardening.jpg',
+  description: 'Restore brilliance to your sofas with expert sofa set cleaning. 🛋️✨'
+},
+{
+  id: '5',
+  name: 'Lawm mowing',
+  img: './assets/gardening.jpg',
+  description: 'Trimming lawns to perfection for a pristine outdoor paradise. 🌿🌳'
+},
+{
+  id: '6',
+  name: 'Garden Maintenance',
+  img: './assets/gardening.jpg',
+  description: 'Nurturing gardens with care for a blossoming outdoor sanctuary. 🌼🍃'
+},
+{
+  id: '7',
+  name: 'Tree Cutting',
+  img: './assets/gardening.jpg',
+  description: 'Safely shaping landscapes with precision through expert tree cutting services. 🌳🪓'
+},
+{
+  id: '8',
+  name: 'Grass Planting',
+  img: './assets/gardening.jpg',
+  description: 'Lush green carpets bloom as we sow life into landscapes. 🌱🌿'
+}]
