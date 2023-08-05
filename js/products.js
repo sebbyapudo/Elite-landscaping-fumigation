17acfbaa03e264615faf2f42e9e0a0b0e81e4afb
sebbyapudo <sebbyapudo@gmail.com>
2023-08-05 04:17:21 +0300

    Updated the products array

--- js/products.js
+++ js/products.js
@@ -6,49 +6,49 @@ const products = [{
 },
 {
   id: '2',
-  img: './assets/pemba-green.jpg',
+  img: './assets/variegated-pemba.jpeg',
   name: 'Variegated pemba',
   price: '1300/= per sac',
 },
 {
   id: '3',
-  img: './assets/lawn-mower.jpg',
+  img: './assets/bermuda-grass-seeds.jpg',
   name: 'Bermuda grass seeds',
   price: '4500/= 0.5kg',
 },
 {
   id: '4',
-  img: './assets/lawn-mower.jpg',
+  img: './assets/arabic-grass.jpeg',
   name: 'Arabic grass seeds',
   price: '1500/= per sac',
 },
 {
   id: '5',
-  img: './assets/lawn-mower.jpg',
+  img: './assets/bracharia.jpg',
   name: 'Zimbabwe grass seeds',
   price: '1300/= 0.5kg',
 },
 {
   id: '6',
-  img: './assets/lawn-mower.jp',
+  img: './assets/papsalum.jpeg',
   name: 'Paspalum grass seeds',
   price: '5000/= 0.5kg',
 },
 {
   id: '7',
-  img: './assets/lawn-mower.jpg',
+  img: './assets/kikuyu.jpg',
   name: 'Kikuyu grass seeds',
   price: '1300/= 0.5kg',
 },
 {
   id: '8',
-  img: './assets/lawn-mower.jpg',
+  img: './assets/bent-grass.jpg',
   name: 'Bent grass seeds',
   price: '7000/=  0.5kg'
 }, 
 {
   id: '9',
-  img: './assets/lawn-mower.jpg',
+  img: './assets/evergreen.webp',
   name: 'Evergreen grass seeds',
   price: '5000/=  0.5kg'
 }]
